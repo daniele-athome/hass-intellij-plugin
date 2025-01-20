718c103a13e69fa38edbe075e34be4b733361f0a
Daniele Ricci <daniele@casaricci.it>
2025-01-20 19:40:20 +0100

    Reuse UI code from another package

--- src/main/kotlin/it/casaricci/hass/plugin/facet/HassFacetEditorTab.kt
+++ src/main/kotlin/it/casaricci/hass/plugin/facet/HassFacetEditorTab.kt
@@ -1,31 +1,21 @@
 package it.casaricci.hass.plugin.facet
 
 import com.intellij.facet.ui.*
-import com.intellij.icons.AllIcons
 import com.intellij.openapi.options.ConfigurationException
 import com.intellij.openapi.util.text.StringUtil
 import com.intellij.util.Urls
-import com.intellij.util.ui.FormBuilder
-import com.intellij.util.ui.JBUI
 import it.casaricci.hass.plugin.MyBundle
 import it.casaricci.hass.plugin.services.HassRemoteRepository
 import org.jetbrains.annotations.Nls
-import java.awt.BorderLayout
-import javax.swing.JButton
 import javax.swing.JComponent
-import javax.swing.JPanel
-import javax.swing.JTextField
 
-// TODO use HassFacetSettingsComponent
 class HassFacetEditorTab(
     private val state: HassFacetState,
     private val context: FacetEditorContext,
     private val manager: FacetValidatorsManager
 ) : FacetEditorTab() {
 
-    private val instanceUrlField = JTextField(state.instanceUrl)
-    private val tokenField = JTextField(state.token)
-    private val refreshButton = JButton(MyBundle.message("hass.facet.editor.refresh.text"), AllIcons.Actions.Refresh)
+    private lateinit var component: HassFacetSettingsComponent
 
     @Nls(capitalization = Nls.Capitalization.Title)
     override fun getDisplayName(): String {
@@ -33,25 +23,15 @@ class HassFacetEditorTab(
     }
 
     override fun createComponent(): JComponent {
-        // TODO add a button for refreshing the cached JSONs (services, entities, etc.)
-        val contentPanel = FormBuilder.createFormBuilder()
-            .addLabeledComponent(
-                MyBundle.message("hass.facet.editor.instanceUrl.title"),
-                instanceUrlField
-            )
-            .addLabeledComponent(
-                MyBundle.message("hass.facet.editor.token.title"),
-                tokenField
-            )
-            .addComponent(refreshButton)
-            .panel.apply {
-                border = JBUI.Borders.empty(10)
-            }
+        component = HassFacetSettingsComponent().apply {
+            instanceUrl = state.instanceUrl
+            token = state.token
+        }
 
         manager.registerValidator(object : FacetEditorValidator() {
             override fun check(): ValidationResult {
                 val url = try {
-                    Urls.parse(instanceUrlField.text, false)
+                    Urls.parse(component.instanceUrl, false)
                 } catch (_: Exception) {
                     null
                 }
@@ -61,37 +41,35 @@ class HassFacetEditorTab(
                     ValidationResult(MyBundle.message("hass.facet.editor.instanceUrl.invalid"))
                 }
             }
-        }, instanceUrlField)
+        }, component.instanceUrlField)
 
         manager.registerValidator(object : FacetEditorValidator() {
             override fun check(): ValidationResult {
-                return if (tokenField.text.trim().isNotEmpty()) {
+                return if (component.token.trim().isNotEmpty()) {
                     ValidationResult.OK
                 } else {
                     ValidationResult(MyBundle.message("hass.facet.editor.token.invalid"))
                 }
             }
-        }, tokenField)
+        }, component.tokenField)
 
-        refreshButton.addActionListener {
+        component.setRefreshButtonListener {
             apply()
         }
 
-        return JPanel(BorderLayout()).apply {
-            add(contentPanel, BorderLayout.NORTH)
-        }
+        return component.panel
     }
 
     override fun isModified(): Boolean {
-        return !StringUtil.equals(state.instanceUrl, instanceUrlField.text.trim()) ||
-                !StringUtil.equals(state.token, tokenField.text.trim())
+        return !StringUtil.equals(state.instanceUrl, component.instanceUrl.trim()) ||
+                !StringUtil.equals(state.token, component.token.trim())
     }
 
     @Throws(ConfigurationException::class)
     override fun apply() {
         try {
-            state.instanceUrl = instanceUrlField.text
-            state.token = tokenField.text
+            state.instanceUrl = component.instanceUrl
+            state.token = component.token
 
             // trigger download immediately
             val service = context.project.getService(HassRemoteRepository::class.java)
@@ -103,9 +81,8 @@ class HassFacetEditorTab(
     }
 
     override fun reset() {
-        instanceUrlField.text = state.instanceUrl
-        tokenField.text = state.token
+        component.instanceUrl = state.instanceUrl
+        component.token = state.token
     }
-
 
 }
--- src/main/kotlin/it/casaricci/hass/plugin/facet/HassFacetSettingsComponent.kt
+++ src/main/kotlin/it/casaricci/hass/plugin/facet/HassFacetSettingsComponent.kt
@@ -1,17 +1,23 @@
 package it.casaricci.hass.plugin.facet
 
+import com.intellij.icons.AllIcons
 import com.intellij.util.ui.FormBuilder
 import com.intellij.util.ui.JBUI
 import it.casaricci.hass.plugin.MyBundle
 import java.awt.BorderLayout
+import javax.swing.JButton
 import javax.swing.JPanel
 import javax.swing.JTextField
 
 class HassFacetSettingsComponent {
 
-    private val instanceUrlField = JTextField()
-    private val tokenField = JTextField()
-    // TODO private val refreshButton = JButton(MyBundle.message("hass.facet.editor.refresh.text"), AllIcons.Actions.Refresh)
+    /** We need public access for the facet editor tab. */
+    val instanceUrlField = JTextField()
+
+    /** We need public access for the facet editor tab. */
+    val tokenField = JTextField()
+
+    private val refreshButton = JButton(MyBundle.message("hass.facet.editor.refresh.text"), AllIcons.Actions.Refresh)
 
     val panel: JPanel
     val preferredFocusedComponent = instanceUrlField
@@ -26,7 +32,7 @@ class HassFacetSettingsComponent {
                 MyBundle.message("hass.facet.editor.token.title"),
                 tokenField
             )
-            // TODO .addComponent(refreshButton)
+            .addComponent(refreshButton)
             .panel.apply {
                 border = JBUI.Borders.empty(10)
             }
@@ -35,6 +41,12 @@ class HassFacetSettingsComponent {
 
         panel = JPanel(BorderLayout()).apply {
             add(contentPanel, BorderLayout.NORTH)
+        }
+    }
+
+    fun setRefreshButtonListener(listener: () -> Unit) {
+        refreshButton.addActionListener {
+            listener()
         }
     }
 
