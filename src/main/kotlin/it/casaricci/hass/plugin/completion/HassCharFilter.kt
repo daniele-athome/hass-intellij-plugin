5967b81a612a8941fa21c4be721e78db74416212
Daniele Ricci <daniele@casaricci.it>
2025-01-21 22:41:13 +0100

    CharFilter is ok for now

--- src/main/kotlin/it/casaricci/hass/plugin/completion/HassCharFilter.kt
+++ src/main/kotlin/it/casaricci/hass/plugin/completion/HassCharFilter.kt
@@ -9,7 +9,6 @@ class HassCharFilter : CharFilter() {
 
     override fun acceptChar(c: Char, prefixLength: Int, lookup: Lookup?): Result? {
         if (lookup?.psiFile?.language == YAMLLanguage.INSTANCE && lookup?.psiElement?.nextSibling is YAMLScalar) {
-            // TODO make this decision for other characters in a YAML string literal
             // "." is for entity ID: "sensor.name"
             // ":" is for icon names: "mdi:home"
             if (c == '.' || c == ':') {
