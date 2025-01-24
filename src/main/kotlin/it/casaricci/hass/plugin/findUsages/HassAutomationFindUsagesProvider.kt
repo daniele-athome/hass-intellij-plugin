78cc13334ad3ad588f4a65a58b99c2a51c36a7fa
Daniele Ricci <daniele@casaricci.it>
2025-01-24 13:55:21 +0100

    Experimenting with find usages for automation (wip) [skip ci]

---
+++ src/main/kotlin/it/casaricci/hass/plugin/findUsages/HassAutomationFindUsagesProvider.kt
@@ -0,0 +1,71 @@
+package it.casaricci.hass.plugin.findUsages
+
+import com.intellij.lang.cacheBuilder.WordsScanner
+import com.intellij.lang.findUsages.FindUsagesProvider
+import com.intellij.openapi.diagnostic.thisLogger
+import com.intellij.psi.PsiElement
+import com.intellij.psi.util.parentOfType
+import it.casaricci.hass.plugin.HassKnownDomains
+import it.casaricci.hass.plugin.MyBundle
+import org.jetbrains.yaml.YAMLWordsScanner
+import org.jetbrains.yaml.psi.YAMLKeyValue
+import org.jetbrains.yaml.psi.YAMLSequence
+import org.jetbrains.yaml.psi.YAMLSequenceItem
+
+class HassAutomationFindUsagesProvider : FindUsagesProvider {
+
+    override fun getWordsScanner(): WordsScanner {
+        return YAMLWordsScanner()
+    }
+
+    override fun canFindUsagesFor(element: PsiElement): Boolean {
+        thisLogger().trace("canFindUsageFor: $element")
+        return isMyElement(element)
+    }
+
+    override fun getHelpId(element: PsiElement): String? {
+        return null
+    }
+
+    override fun getType(element: PsiElement): String {
+        if (isMyElement(element)) {
+            return MyBundle.message("hass.findUsages.haAutomation")
+        }
+        return ""
+    }
+
+    override fun getDescriptiveName(element: PsiElement): String {
+        if (isMyElement(element)) {
+            return element.text
+        }
+        return ""
+    }
+
+    override fun getNodeText(element: PsiElement, useFullName: Boolean): String {
+        if (isMyElement(element)) {
+            return element.text + ":"
+        }
+        return ""
+    }
+
+    private fun isMyElement(element: PsiElement): Boolean {
+        thisLogger().trace("element: $element")
+        val parent = if (element is YAMLKeyValue) {
+            thisLogger().trace("element.keyText: " + element.keyText)
+            element.parentMapping
+        } else {
+            thisLogger().trace("element.text: " + element.text)
+            element.parentOfType<YAMLKeyValue>()?.parentMapping
+        }
+        thisLogger().trace(
+            "lookup parent.keyText: " + parent?.parentOfType<YAMLSequenceItem>()
+                ?.parentOfType<YAMLSequence>()
+                ?.parentOfType<YAMLKeyValue>()
+                ?.keyText
+        )
+        return parent?.parentOfType<YAMLSequenceItem>()
+            ?.parentOfType<YAMLSequence>()
+            ?.parentOfType<YAMLKeyValue>()
+            ?.keyText == HassKnownDomains.AUTOMATION
+    }
+}
