79dd3c55551f5782c7acf6773f07fad57f740629
Daniele Ricci <daniele@casaricci.it>
2025-12-10 22:33:06 +0100

    Workaround probable platform bug

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     }
 
     testImplementation(libs.junit)
+    testImplementation(libs.kotlinStdlib)
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
