f6eef6da1606845f7e73b58d67e3c75e38863955
Daniele Ricci <daniele@casaricci.it>
2025-12-10 19:54:52 +0100

    Code reformat

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,10 +43,12 @@ dependencies {
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
-        intellijIdea(providers.gradleProperty("platformVersion"),
+        intellijIdea(
+            providers.gradleProperty("platformVersion"),
             configure = {
                 useInstaller = providers.gradleProperty("platformUseInstaller").map { it != "false" }.orElse(false)
-            })
+            },
+        )
 
         // Plugin Dependencies. Uses `platformBundledPlugins` property from the gradle.properties file for bundled IntelliJ Platform plugins.
         bundledPlugins(providers.gradleProperty("platformBundledPlugins").map { it.split(',') })
