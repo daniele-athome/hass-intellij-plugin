c9494908c4069a0689cb50f4c41151e4cc67365d
Daniele Ricci <daniele@casaricci.it>
2026-03-29 15:43:44 +0200

    Upgrade to latest platform version

--- src/main/kotlin/it/casaricci/hass/plugin/completion/HassCharFilter.kt
+++ src/main/kotlin/it/casaricci/hass/plugin/completion/HassCharFilter.kt
@@ -6,10 +6,10 @@ import org.jetbrains.yaml.YAMLLanguage
 import org.jetbrains.yaml.psi.YAMLScalar
 
 class HassCharFilter : CharFilter() {
-    override fun acceptChar(c: Char, prefixLength: Int, lookup: Lookup?): Result? {
+    override fun acceptChar(c: Char, prefixLength: Int, lookup: Lookup): Result? {
         if (
-            lookup?.psiFile?.language == YAMLLanguage.INSTANCE &&
-                lookup?.psiElement?.nextSibling is YAMLScalar
+            lookup.psiFile?.language == YAMLLanguage.INSTANCE &&
+                lookup.psiElement?.nextSibling is YAMLScalar
         ) {
             // "." is for entity ID: "sensor.name"
             // ":" is for icon names: "mdi:home"
