7fa2b6e2aa1cad8adbc52b5b7e24d443763ee047
Daniele Ricci <daniele@casaricci.it>
2026-03-29 15:43:27 +0200

    Another tentative compatibility fix
    This is not going to work in the long run...

--- src/main/kotlin/it/casaricci/hass/plugin/compat.kt
+++ src/main/kotlin/it/casaricci/hass/plugin/compat.kt
@@ -1,14 +1,21 @@
 package it.casaricci.hass.plugin
 
+import com.intellij.concurrency.IntelliJContextElement
 import com.intellij.openapi.application.ModalityState
-import com.intellij.openapi.application.contextModality
 import com.intellij.openapi.progress.*
+import com.intellij.platform.util.progress.ProgressReporter
 import com.intellij.platform.util.progress.RawProgressReporter
-import com.intellij.platform.util.progress.internalCreateRawHandleFromContextStepIfExistsAndFresh
+import com.intellij.platform.util.progress.RawProgressReporterHandle
+import com.intellij.platform.util.progress.SequentialProgressReporter
+import com.intellij.platform.util.progress.StepState
+import com.intellij.platform.util.progress.impl.ProgressText
+import kotlin.coroutines.AbstractCoroutineContextElement
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.cancellation.CancellationException
 import kotlin.coroutines.coroutineContext
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.ensureActive
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.job
 
 /** Compatibility function for [coroutineToIndicator]. */
@@ -22,12 +29,50 @@ suspend fun <T> coroutineToIndicatorCompat(action: (ProgressIndicator?) -> T): T
     }
 }
 
+internal sealed interface SequentialProgressReporterHandle : AutoCloseable {
+
+    val reporter: SequentialProgressReporter
+
+    override fun close()
+}
+
+internal sealed interface ProgressReporterHandle : AutoCloseable {
+
+    val reporter: ProgressReporter
+
+    override fun close()
+}
+
+internal sealed interface ProgressStep {
+
+    fun progressUpdates(): Flow<StepState>
+
+    suspend fun <X> withText(text: ProgressText, action: suspend CoroutineScope.() -> X): X
+
+    fun asConcurrent(size: Int): ProgressReporterHandle?
+
+    fun asSequential(size: Int): SequentialProgressReporterHandle?
+
+    fun asRaw(): RawProgressReporterHandle?
+}
+
+private class ProgressStepElement(val step: ProgressStep) :
+    AbstractCoroutineContextElement(Key), IntelliJContextElement {
+    override fun produceChildElement(
+        parentContext: CoroutineContext,
+        isStructured: Boolean,
+    ): IntelliJContextElement = this
+
+    object Key : CoroutineContext.Key<ProgressStepElement>
+}
+
+/** Modified to not use internal APIs. */
 @Throws(CancellationException::class)
 private fun <T> contextToIndicator(ctx: CoroutineContext, action: () -> T): T {
     val job = ctx.job
     job.ensureActive()
-    val contextModality = ctx.contextModality() ?: ModalityState.nonModal()
-    val handle = ctx.internalCreateRawHandleFromContextStepIfExistsAndFresh()
+    val contextModality = ModalityState.nonModal()
+    val handle = ctx[ProgressStepElement.Key]?.step?.asRaw()
     return if (handle != null) {
         handle.use {
             val indicator = RawProgressReporterIndicator(handle.reporter, contextModality)
